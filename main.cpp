2263085e4449138bd32cc06c0a6043aca917e87f
JerryMiao2019 <Jerrymiao2019@outlook.com>
2022-03-05 20:38:02 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int returnFor(int n)
+{
+    int m = n * n;
+    return m;
+}
+
+int main()
+{
+    using namespace std;
+    cout << returnFor(10192 * 8);
+    return 0;
+}
+
